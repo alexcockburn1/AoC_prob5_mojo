27de3f2f4fd84f14e6447b2b2be08250b7d61885
Alexander Cockburn <alexa@Alexanders-MacBook-Air.local>
2024-04-27 15:32:35 +0100

    Tidy up Python solution

--- prob5.py
+++ prob5.py
@@ -1,5 +1,4 @@
 from typing import List
-import math
 import time
 
 
@@ -40,10 +39,34 @@ class OverallMapping:
             mapping = self.mappings[i]
             mapped_number = mapping.apply_mapping(mapped_number)
         return mapped_number
+    
+
+def part5a(overall_mapping, seeds_int):
+    minimum = 1000000000000
+    for i in range(len(seeds_int)):
+        seed = seeds_int[i]
+        transformed_number = overall_mapping.apply_mappings(seed)
+        if transformed_number < minimum:
+            minimum = transformed_number
+    print(str("minimum"))
+
+
+def part5b(overall_mapping, seeds_int):
+    total_numbers_to_check = sum([seed for i, seed in enumerate(seeds_int) if i % 2 != 0])
+    print(f"{total_numbers_to_check=}")
+
+    minimum = 1000000000000
+    for seed_index in range(0, len(seeds_int), 2):
+        for i in range(seeds_int[seed_index], seeds_int[seed_index] + seeds_int[seed_index + 1]):
+            transformed_number = overall_mapping.apply_mappings(i)
+            if transformed_number < minimum:
+                minimum = transformed_number
+        print("Finished seed_index " + str(seed_index))
+    print(str(minimum))
 
 
 def main():
-    with open("test_file.txt", "r") as handle:
+    with open("test_file_small.txt", "r") as handle:
         text = handle.read()
         lines = text.split("\n")
         seeds_line = lines[0]
@@ -58,34 +81,10 @@ def main():
                 row_vals = parse_list_of_ints(line)
                 mappings[-1].intervals.append(ShiftInterval(source_start=row_vals[1], source_end=row_vals[1] + row_vals[2], destination_start=row_vals[0]))
         overall_mapping = OverallMapping(mappings)
-        
-        # minimum = 1000000000000  # sucks that I can't do math.inf here
-        # for i in range(len(seeds_int)): # I hate doing for loops like this so much!
-        #     seed = seeds_int[i]
-        #     transformed_number = overall_mapping.apply_mappings(seed)
-        #     if transformed_number < minimum:
-        #         minimum = transformed_number
-        # # 5a solution
-        # print(minimum)
 
-        total_numbers_to_check = sum([seed for i, seed in enumerate(seeds_int) if i % 2 != 0])
-        print(f"{total_numbers_to_check=}")
+        part5a(overall_mapping, seeds_int)
+        part5b(overall_mapping, seeds_int)
 
-        minimum = 1000000000000
-        for seed_index in range(0, len(seeds_int), 2):
-            start_time = time.perf_counter()
-            for i in range(seeds_int[seed_index], seeds_int[seed_index] + seeds_int[seed_index + 1]):
-                if i - seeds_int[seed_index] == 1000000:
-                    print(f"Completed {i - seeds_int[seed_index]} in time {time.perf_counter() - start_time}")
-
-                transformed_number = overall_mapping.apply_mappings(i)
-                if transformed_number < minimum:
-                    minimum = transformed_number
-            print("Finished seed_index " + str(seed_index))
-        print("5b solutions is: " + str(minimum))
-
-        # number_iters = sum([val for i, val in enumerate(seeds_int) if i % 2 == 0])
-        # print(number_iters)
 
 if __name__ == "__main__":
     main()
